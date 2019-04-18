1092bbd60dd58cb8479890a1ccfbfa9cac4f817d
CoutinhoElias <coutinho.elias@gmail.com>
2019-04-18 11:12:13 -0300

    Melhoria na apresentação do Admin.

--- pesquisasatisfacao/crm/admin.py
+++ pesquisasatisfacao/crm/admin.py
@@ -5,12 +5,23 @@ from pesquisasatisfacao.crm.models import Typeofservice, Atendimento
 
 @admin.register(Typeofservice)
 class TypeofserviceAdmin(admin.ModelAdmin):
-    list_display = ('__str__', )
+    list_display = ('id', 'name', 'user_destination')
     # search_fields = ('cdalterdata', 'name', 'city', 'last_search')
     # filter_horizontal = ('qualification',)
 
 
 @admin.register(Atendimento)
 class AtendimentoAdmin(admin.ModelAdmin):
-    list_display = ('__str__', )
+    list_display = ('next_date',
+                    'type',
+                    'department',
+                    'person',
+                    'product',
+                    'priority',
+                    'contact',
+                    'feedback',
+                    'created_on',
+                    'deadline',
+                    'user',
+                    'closed')
     # search_fields = ('cdalterdata', 'name', 'city', 'last_search')
